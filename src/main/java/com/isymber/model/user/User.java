e905cb054382857f963a2e161d8650eb77069ea3
Kamil <kamil.lobasiuk@gmailc.om>
2021-05-25 11:37:23 +0200

    created abstract model & user model, get connection with db

---
+++ src/main/java/com/isymber/model/user/User.java
@@ -0,0 +1,65 @@
+package com.isymber.model.user;
+
+
+import com.isymber.model.AbstractModel;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@AllArgsConstructor
+public class User extends AbstractModel implements UserDetails {
+
+    @NotNull
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String login;
+    private Role role;
+
+    public User() {
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
