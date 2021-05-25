62526279585467a3491b38325a7599f1aed596ec
Kamil <kamil.lobasiuk@gmailc.om>
2021-05-25 11:36:32 +0200

    init project

---
+++ src/main/java/com/isymber/GalleryAppVaadinApplication.java
@@ -0,0 +1,13 @@
+package com.isymber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryAppVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryAppVaadinApplication.class, args);
+    }
+
+}
